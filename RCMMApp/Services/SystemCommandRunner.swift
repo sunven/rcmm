24e07cb43ece5eb9a90d03832e435f2240ca9ef5
sunven <604164227@qq.com>
2026-04-25 16:55:30 +0800

    Drain process pipes while commands run

--- RCMMApp/Services/SystemCommandRunner.swift
+++ RCMMApp/Services/SystemCommandRunner.swift
@@ -10,6 +10,29 @@ protocol SystemCommandRunning: Sendable {
     func run(executable: URL, arguments: [String]) throws -> SystemCommandResult
 }
 
+private final class ProcessPipeReader: @unchecked Sendable {
+    private let handle: FileHandle
+    private let group = DispatchGroup()
+    private var data = Data()
+
+    init(handle: FileHandle) {
+        self.handle = handle
+    }
+
+    func start() {
+        group.enter()
+        DispatchQueue.global(qos: .userInitiated).async { [self] in
+            data = handle.readDataToEndOfFile()
+            group.leave()
+        }
+    }
+
+    func waitForData() -> Data {
+        group.wait()
+        return data
+    }
+}
+
 struct SystemCommandRunner: SystemCommandRunning {
     func run(executable: URL, arguments: [String]) throws -> SystemCommandResult {
         let process = Process()
@@ -21,11 +44,32 @@ struct SystemCommandRunner: SystemCommandRunning {
         process.standardOutput = stdoutPipe
         process.standardError = stderrPipe
 
-        try process.run()
+        let stdoutReader = ProcessPipeReader(handle: stdoutPipe.fileHandleForReading)
+        let stderrReader = ProcessPipeReader(handle: stderrPipe.fileHandleForReading)
+        stdoutReader.start()
+        stderrReader.start()
+
+        defer {
+            stdoutPipe.fileHandleForReading.closeFile()
+            stderrPipe.fileHandleForReading.closeFile()
+        }
+
+        do {
+            try process.run()
+            stdoutPipe.fileHandleForWriting.closeFile()
+            stderrPipe.fileHandleForWriting.closeFile()
+        } catch {
+            stdoutPipe.fileHandleForWriting.closeFile()
+            stderrPipe.fileHandleForWriting.closeFile()
+            _ = stdoutReader.waitForData()
+            _ = stderrReader.waitForData()
+            throw error
+        }
+
         process.waitUntilExit()
 
-        let stdoutData = stdoutPipe.fileHandleForReading.readDataToEndOfFile()
-        let stderrData = stderrPipe.fileHandleForReading.readDataToEndOfFile()
+        let stdoutData = stdoutReader.waitForData()
+        let stderrData = stderrReader.waitForData()
 
         return SystemCommandResult(
             stdout: String(decoding: stdoutData, as: UTF8.self),
